a77bc8c3ee64bb556dd2ade023ccf844f5559af8
Tjcarroll1 <tjcarroll1@icloud.com>
2023-05-12 17:04:15 +1000

    added code to try an fix error but now [resented with new error in the product-routes.js

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -40,6 +40,15 @@ router.post('/', async (req, res) => {
       stock: 3,
       tagIds: [1, 2, 3, 4]
     });
+    if (req.body.tagIds.length) {
+      const productTagIdArr = req.body.tagIds.map((tag_id) => {
+        return {
+          product_id: product.id,
+          tag_id,
+        };
+      });
+      await ProductTag.bulkCreate(productTagIdArr);
+    }
     res.status(200).json(productData);
   }
   catch (err) {
@@ -48,26 +57,6 @@ router.post('/', async (req, res) => {
 });
 
 
-  Product.create(req.body)
-    .then((product) => {
-      // if there's product tags, we need to create pairings to bulk create in the ProductTag model
-      if (req.body.tagIds.length) {
-        const productTagIdArr = req.body.tagIds.map((tag_id) => {
-          return {
-            product_id: product.id,
-            tag_id,
-          };
-        });
-        return ProductTag.bulkCreate(productTagIdArr);
-      }
-      // if no product tags, just respond
-      res.status(200).json(product);
-    })
-    .then((productTagIds) => res.status(200).json(productTagIds))
-    .catch((err) => {
-      console.log(err);
-      res.status(400).json(err);
-    });
 
 
 // update product
