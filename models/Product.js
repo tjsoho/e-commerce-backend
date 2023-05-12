94a9e1e109e5767096ccc891eb5577883bd7387d
Tjcarroll1 <tjcarroll1@icloud.com>
2023-05-12 18:55:32 +1000

    added models

--- models/Product.js
+++ models/Product.js
@@ -4,13 +4,53 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
-class Product extends Model {}
+class Product extends Model { }
 
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+
+    price: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      // set default value of 10
+      defaultValue: 10,
+      // validate that value is numeric
+      validate: {
+        isNumeric: true
+      }
+    },
+
+    category_id: {
+      type: DataTypes.INTEGER,
+      // references the category model's id
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    }
   },
+
+
   {
     sequelize,
     timestamps: false,
